8bd48a840c5da62effcf0c22a54aeab832e353c9
JeremyEspinoza3912 <43081346+JeremyEspinoza3912@users.noreply.github.com>
2019-03-11 22:18:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 heights = pd.read_csv('average-height-of-men-for-selected-countries.csv')
 
 peru_heights = heights[heights['Entity'] == 'Peru']
 peru_heights.columns = ['Entity','Code','Year','Heights']
-peru_heights.plot(x = 'Entity',y = 'Heights',kind = 'scatter')
+peru_heights.plt.plot(x = 'Entity',y = 'Heights',kind = 'line')
+
